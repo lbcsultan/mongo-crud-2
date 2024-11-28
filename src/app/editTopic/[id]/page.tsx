15dd56b9a08987f7280e050917c65f193a447f5c
Byoungcheon <lbcsultan@gmail.com>
2024-11-28 17:27:59 +0900

    2nd

--- src/app/editTopic/[id]/page.tsx
+++ src/app/editTopic/[id]/page.tsx
@@ -1,4 +1,6 @@
+import { auth } from '@/auth'
 import EditTopicForm from '@/components/EditTopicForm'
+import { redirect } from 'next/navigation'
 
 const apiUrl = process.env.API_URL
 
@@ -21,8 +23,14 @@ export default async function EditTopic({
 }: {
   params: { id: string }
 }) {
+  const session = await auth()
+  if (!session) {
+    redirect('/login')
+  }
+
   const { id } = await params
   const { topic } = await getTopicById(id)
+  // console.log(topic)
   const { title, description } = topic
 
   return <EditTopicForm id={id} title={title} description={description} />
